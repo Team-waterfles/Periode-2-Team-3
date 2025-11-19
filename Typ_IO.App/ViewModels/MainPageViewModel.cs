b096959fe30d02f426d0fbc32ca7351daf593772
bobot3 <bo.kulman@icloud.com>
2025-11-19 17:11:09 +0100

    added mainPage with settings and play + exit button (non functional since there isnt a 'play' view yet

---
+++ Typ_IO.App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Maui.Controls;
+using System.Windows.Input;
+
+namespace BasisJaar2.ViewModels
+{
+    public class MainPageViewModel : BindableObject
+    {
+        private ContentView _subpageContent;
+        public ContentView SubpageContent
+        {
+            get => _subpageContent;
+            set
+            {
+                _subpageContent = value;
+                OnPropertyChanged();
+                UpdateToolbarProperties();
+            }
+        }
+
+        private bool _isInSettings => SubpageContent is Views.Settings;
+
+        private string _toolbarIcon;
+        public string ToolbarIcon
+        {
+            get => _toolbarIcon;
+            set { _toolbarIcon = value; OnPropertyChanged(); }
+        }
+
+        private string _toolbarText;
+        public string ToolbarText
+        {
+            get => _toolbarText;
+            set { _toolbarText = value; OnPropertyChanged(); }
+        }
+
+        public ICommand ToolbarCommand { get; }
+
+        public MainPageViewModel()
+        {
+            SubpageContent = new Views.StartScreen();
+            ToolbarCommand = new Command(OnToolbarClicked);
+            UpdateToolbarProperties();
+        }
+
+        private void OnToolbarClicked()
+        {
+            if (_isInSettings)
+                SubpageContent = new Views.StartScreen();
+            else
+                SubpageContent = new Views.Settings();
+        }
+
+        private void UpdateToolbarProperties()
+        {
+            if (_isInSettings)
+            {
+                ToolbarIcon = null;           // Important for text-only button
+                ToolbarText = "Exit";          // Must be non-empty
+            }
+            else
+            {
+                ToolbarIcon = "settings.png";  // Icon for default state
+                ToolbarText = null;            // No text
+            }
+        }
+    }
+}
