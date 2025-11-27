cbdf3aa919c8bd04b3f4ed5a7974352ac6c82762
Michael <s1197491@student.windesheim.nl>
2025-11-27 15:50:57 +0100

    MySqlConnectionFactory aangemaakt

---
+++ Typ_IO.Core.Data/MySqlConnectionFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySqlConnector;
+
+namespace Typ_IO.Core.Data
+{
+    public class MySqlConnectionFactory
+    {
+        private readonly string _connectionString;
+        public MySqlConnectionFactory()
+        {
+            MySqlConnection _connectionString = new MySqlConnection(
+                "server=localhost;" +
+                "user=???;" +
+                "database=???;" +
+                "port=3306;" +
+                "password=???");
+        }
+        public async Task<MySqlConnection> CreateOpenConnectionAsync()
+        {
+            var conn = new MySqlConnection(_connectionString);
+            await conn.OpenAsync();
+            return conn;
+        }
+    }
+}
