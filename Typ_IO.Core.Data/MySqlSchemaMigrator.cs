5dadc83077f56847d5184669670ea5083c199caa
Michael <s1197491@student.windesheim.nl>
2025-11-27 16:00:00 +0100

    MySqlSchemaMigrator toegevoegd

---
+++ Typ_IO.Core.Data/MySqlSchemaMigrator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typ_IO.Core.Data
+{
+    public class MySqlSchemaMigrator
+    {
+        private readonly MySqlConnectionFactory _factory;
+        public MySqlSchemaMigrator(MySqlConnectionFactory factory) => _factory = factory;
+
+        public async Task MigrateAsync()
+        {
+            using var conn = await _factory.CreateOpenConnectionAsync();
+            using var cmd = conn.CreateCommand();
+
+            cmd.CommandText = @"
+                CREATE TABLE IF NOT EXISTS `Level` (
+                  `Id` INT UNSIGNED NOT NULL,
+                  `Naam` VARCHAR(50) NOT NULL,
+                  `Tekst` VARCHAR(2000) NOT NULL,
+                  `Moeilijkheidsgraad` ENUM('MAKKELIJK', 'NORMAAL', 'MOEILIJK') NOT NULL,
+                  `Type` ENUM('OEFENING', 'EXAMEN') NOT NULL,
+                  `NummerBestand` VARCHAR(100) NULL,
+                  `MinimumScore` INT UNSIGNED NULL,
+                PRIMARY KEY (`Id`),
+                UNIQUE INDEX `Id_UNIQUE` (`Id` ASC));
+                ";
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }
+}
