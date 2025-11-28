cb1d01d24c02a4d157a2b78e81ca20a858beca7d
Michael <s1197491@student.windesheim.nl>
2025-11-28 12:10:14 +0100

    Sqlite database toegevoegd in Core.data

---
+++ Typ_IO.Core.Data/SqliteSchemaMigrator.cs
@@ -0,0 +1,29 @@
+﻿namespace Typ_IO.Core.Data
+{
+    public class SqliteSchemaMigrator
+    {
+        private readonly ISqliteConnectionFactory _factory;
+        public SqliteSchemaMigrator(ISqliteConnectionFactory factory) => _factory = factory;
+
+        public async Task MigrateAsync()
+        {
+            using var conn = await _factory.CreateOpenConnectionAsync();
+            using var cmd = conn.CreateCommand();
+
+            cmd.CommandText = @"
+            CREATE TABLE IF NOT EXISTS `Level` (
+              `Id` INT UNSIGNED NOT NULL,
+              `Naam` VARCHAR(50) NOT NULL,
+              `Tekst` VARCHAR(2000) NOT NULL,
+              `Moeilijkheidsgraad` ENUM('MAKKELIJK', 'NORMAAL', 'MOEILIJK') NOT NULL,
+              `Type` ENUM('OEFENING', 'EXAMEN') NOT NULL,
+              `NummerBestand` VARCHAR(100) NULL,
+              `MinimumScore` INT UNSIGNED NULL,
+            PRIMARY KEY (`Id`),
+            UNIQUE INDEX `Id_UNIQUE` (`Id` ASC));
+            ";
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }
+
+}
