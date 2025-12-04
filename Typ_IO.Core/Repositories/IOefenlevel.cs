adb180bed1f7949a864f18c9ad31b402ecc5f7a4
Michael <s1197491@student.windesheim.nl>
2025-12-04 14:48:43 +0100

    Repository-interfaces toegevoegd

---
+++ Typ_IO.Core/Repositories/IOefenlevel.cs
@@ -0,0 +1,8 @@
+﻿using Typ_IO.Core.Models;
+
+namespace Typ_IO.Core.Repositories
+{
+    internal interface IOefenlevel : IRepository<Oefenlevel>
+    {
+    }
+}
---
+++ Typ_IO.Core/Repositories/IStandaardlevel.cs
@@ -0,0 +1,8 @@
+﻿using Typ_IO.Core.Models;
+
+namespace Typ_IO.Core.Repositories
+{
+    internal interface IStandaardlevel : IRepository<Standaardlevel>
+    {
+    }
+}
