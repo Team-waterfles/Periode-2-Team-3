366390a9777f28188c37e8c8fdfc8966d873d4e7
Michael <s1197491@student.windesheim.nl>
2025-12-09 15:46:45 +0100

    Alle standaardlevel overgezet

--- Typ_IO.App/MauiProgram.cs
+++ Typ_IO.App/MauiProgram.cs
@@ -62,8 +62,12 @@ namespace BasisJaar2
             if ((await standaardlevels.ListAsync()).Count == 0)
             {
                 Standaardlevel[] standaardlevelmodellen = [
-                    new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Oefenlevel", Tekst = "Pa's wijze lynx bezag vroom het fikse aquaduct" },
-                    new Standaardlevel {Moeilijkheidsgraad = 2, Naam = "Oefenlevel", Tekst = "typ deze zin zorgvuldig om je snelheid te testen" }];
+                    new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel1", Tekst = "Pa's wijze lynx bezag vroom het fikse aquaduct" },
+                    new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel2", Tekst = "Typ deze zin zorgvuldig om je snelheid te testen" },
+                    new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel3", Tekst = "Dit is een moeilijkere oefening voor gevorderde typers" },
+                    new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Woordenlijst", Tekst = "Hallo wereld auto fiets computer toetsenbord oefen typen leren school student programmeren muziek ritme spelen piano gitaar drums zingen Nederland Amsterdam Rotterdam Utrecht Groningen" },
+                    new Standaardlevel {Moeilijkheidsgraad = 2, Naam = "Simpele tekst", Tekst = "De kat zit op de mat. De hond rent door het park. Typen leren is leuk en nuttig voor iedereen. Muziek en ritme helpen bij het oefenen. Practice makes perfect, dus blijf oefenen!" },
+                    new Standaardlevel {Moeilijkheidsgraad = 3, Naam = "Geavanceerde tekst", Tekst = "Dit is een uitgebreide typoefening voor gevorderde gebruikers. Typen is een belangrijke vaardigheid in de moderne wereld. Of je nu student bent, professional of hobbyist, goede typvaardigheden maken je werk een stuk efficiënter. Door regelmatig te oefenen met verschillende soorten teksten, verbeter je niet alleen je snelheid maar ook je nauwkeurigheid. Muziek en ritme kunnen helpen om je typritme te verbeteren en het oefenen aangenamer te maken." }];
                 foreach (Standaardlevel level in standaardlevelmodellen)
                 {
                     await standaardlevels.AddAsync(level);
