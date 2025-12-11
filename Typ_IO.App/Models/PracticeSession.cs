dc67159ef1aba03c922fb8e6866456732b72fa01
Michael <s1197491@student.windesheim.nl>
2025-12-11 14:44:53 +0100

    Implementatie practicesession verbeterd door Gehaalde levels bij te houden met een integer i.p.v. een set

--- Typ_IO.App/Models/PracticeSession.cs
+++ Typ_IO.App/Models/PracticeSession.cs
@@ -6,22 +6,22 @@ namespace BasisJaar2.Models;
 public static class PracticeSession
 {
     public static Level? GeselecteerdLevel { get; set; }
-    private static readonly HashSet<int> GehaaldeLevels = new();
+    private static int Voortgang = 0;
 
     public static bool IsLevelUnlocked(int levelNummer)
     {
-        if (levelNummer <= 1) return true;
-        return GehaaldeLevels.Contains(levelNummer - 1);
+        if (levelNummer <= Voortgang + 1) return true;
+        return false;
     }
 
     public static void MarkLevelGehaald(int levelNummer)
     {
-        if (!GehaaldeLevels.Contains(levelNummer))
-            GehaaldeLevels.Add(levelNummer);
+        if (Voortgang < levelNummer)
+        { Voortgang = levelNummer; }
     }
 
     public static bool IsLevelCompleted(int levelNummer)
     {
-        return GehaaldeLevels.Contains(levelNummer);
+        return Voortgang >= levelNummer;
     }
 }
