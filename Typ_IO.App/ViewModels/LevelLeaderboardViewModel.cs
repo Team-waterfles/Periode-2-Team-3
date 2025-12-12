d33c3f5a73dfe100ec8296096c99fbd839be7182
Michael <s1197491@student.windesheim.nl>
2025-12-12 14:06:44 +0100

    Layout xaml maken

--- Typ_IO.App/ViewModels/LevelLeaderboardViewModel.cs
+++ Typ_IO.App/ViewModels/LevelLeaderboardViewModel.cs
@@ -1,8 +1,16 @@
-namespace BasisJaar2.ViewModels;
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using BasisJaar2.Models;
+using BasisJaar2.Views;
+using Typ_IO.Core.Repositories;
+using Typ_IO.Core.Models;
 
+namespace BasisJaar2.ViewModels;
 public partial class LevelLeaderboardViewModel : BindableObject
 {
-	public LevelLeaderboardViewModel()
+    public ObservableCollection<int> LevelScores;
+    public LevelLeaderboardViewModel()
 	{
 	}
 }
