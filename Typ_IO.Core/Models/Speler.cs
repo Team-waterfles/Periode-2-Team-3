fdf6c696ea37134eb0bcc1dba1c25954a80c6889
Michael <s1197491@student.windesheim.nl>
2025-12-12 14:26:10 +0100

    Aanmaken modellen voor spelers en levelscores

---
+++ Typ_IO.Core/Models/Speler.cs
@@ -0,0 +1,8 @@
+﻿namespace Typ_IO.Core.Models
+{
+    public class Speler(int id, string naam)
+    {
+        public int Id { get; private set; } = id;
+        public string Naam { get; set; } = naam;
+    }
+}
---
+++ Typ_IO.Core/Models/SpelerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typ_IO.Core.Models
+{
+    public class SpelerLevel(int speler_id, int level_id, int top_score)
+    {
+        public int SpelerId { get; set; } = speler_id;
+        public int LevelId { get; set; } = level_id;
+        public int TopScore { get; set; } = top_score;
+    }
+}
