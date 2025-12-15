48956a2150313901ebf87f14e184537cd2fd29f5
Michael <s1197491@student.windesheim.nl>
2025-12-15 12:45:29 +0100

    Fucties om de logica die de levels toe voegt aan de db te scheiden van de lijsten met levels

--- Typ_IO.App/MauiProgram.cs
+++ Typ_IO.App/MauiProgram.cs
@@ -58,54 +58,54 @@ namespace BasisJaar2
         }
         private static async Task SeedAsync(IServiceProvider sp)
         {
-            {
-                var levelrepository = sp.GetRequiredService<IStandaardlevelRepository>();
-                Standaardlevel[] levels = [
-                    new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel1", Tekst = "Pa's wijze lynx bezag vroom het fikse aquaduct" },
-                    new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel2", Tekst = "Typ deze zin zorgvuldig om je snelheid te testen" },
-                    new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel3", Tekst = "Dit is een moeilijkere oefening voor gevorderde typers" },
-                    new Standaardlevel {Moeilijkheidsgraad = 2, Naam = "Woorden", Tekst = "Hallo wereld auto fiets computer toetsenbord oefen typen leren school student programmeren muziek ritme spelen piano gitaar drums zingen Nederland Amsterdam Rotterdam Utrecht Groningen" },
-                    new Standaardlevel {Moeilijkheidsgraad = 2, Naam = "Complexere combinaties", Tekst = "De kat zit op de mat. De hond rent door het park. Typen leren is leuk en nuttig voor iedereen. Muziek en ritme helpen bij het oefenen. Practice makes perfect, dus blijf oefenen!" },
-                    new Standaardlevel {Moeilijkheidsgraad = 3, Naam = "Volledige zinnen", Tekst = "Dit is een uitgebreide typoefening voor gevorderde gebruikers. Typen is een belangrijke vaardigheid in de moderne wereld. Of je nu student bent, professional of hobbyist, goede typvaardigheden maken je werk een stuk efficiënter. Door regelmatig te oefenen met verschillende soorten teksten, verbeter je niet alleen je snelheid maar ook je nauwkeurigheid. Muziek en ritme kunnen helpen om je typritme te verbeteren en het oefenen aangenamer te maken." }];
+            Standaardlevel[] standaardlevels = [
+                new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel1", Tekst = "Pa's wijze lynx bezag vroom het fikse aquaduct" },
+                new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel2", Tekst = "Typ deze zin zorgvuldig om je snelheid te testen" },
+                new Standaardlevel {Moeilijkheidsgraad = 1, Naam = "Testlevel3", Tekst = "Dit is een moeilijkere oefening voor gevorderde typers" },
+                new Standaardlevel {Moeilijkheidsgraad = 2, Naam = "Woorden", Tekst = "Hallo wereld auto fiets computer toetsenbord oefen typen leren school student programmeren muziek ritme spelen piano gitaar drums zingen Nederland Amsterdam Rotterdam Utrecht Groningen" },
+                new Standaardlevel {Moeilijkheidsgraad = 2, Naam = "Complexere combinaties", Tekst = "De kat zit op de mat. De hond rent door het park. Typen leren is leuk en nuttig voor iedereen. Muziek en ritme helpen bij het oefenen. Practice makes perfect, dus blijf oefenen!" },
+                new Standaardlevel {Moeilijkheidsgraad = 3, Naam = "Volledige zinnen", Tekst = "Dit is een uitgebreide typoefening voor gevorderde gebruikers. Typen is een belangrijke vaardigheid in de moderne wereld. Of je nu student bent, professional of hobbyist, goede typvaardigheden maken je werk een stuk efficiënter. Door regelmatig te oefenen met verschillende soorten teksten, verbeter je niet alleen je snelheid maar ook je nauwkeurigheid. Muziek en ritme kunnen helpen om je typritme te verbeteren en het oefenen aangenamer te maken." }];
+            await SeedStandaardlevelAsync(sp.GetRequiredService<IStandaardlevelRepository>(), standaardlevels);
 
-                List<Standaardlevel> levels_in_database = levelrepository.ListAsync().Result;
-                foreach (Standaardlevel level in levels)
+            Oefenlevel[] oefenlevels = [
+                new Oefenlevel {Id = 0, Naam = "2 vingers", Letteropties = " fj" },
+                new Oefenlevel {Id = 1, Naam = "4 vingers", Letteropties = " fjdke" },
+                new Oefenlevel {Id = 2, Naam = "6 vingers", Letteropties = " fjdkewslo" },
+                new Oefenlevel {Id = 3, Naam = "8 vingers", Letteropties = " ;fjdkewsloa" },
+                new Oefenlevel {Id = 4, Naam = "letters bovenste rij", Letteropties = " qwertyuiop" },
+                new Oefenlevel {Id = 5, Naam = "10 vingers", Letteropties = " ;qwertyuiopasdfghjklzxcvbnm" }];
+            await SeedOefenlevelAsync(sp.GetRequiredService<IOefenlevelRepository>(), oefenlevels);
+        }
+        private static async Task SeedStandaardlevelAsync(IStandaardlevelRepository levelrepository, Standaardlevel[] levels)
+        {
+            List<Standaardlevel> levels_in_database = levelrepository.ListAsync().Result;
+            foreach (Standaardlevel level in levels)
+            {
+                Standaardlevel? orginele_level = levels_in_database.Find(l => l.Naam == level.Naam);
+                if (orginele_level == null)
                 {
-                    Standaardlevel? orginele_level = levels_in_database.Find(l => l.Naam == level.Naam);
-                    if (orginele_level == null)
-                    {
-                        await levelrepository.AddAsync(level);
-                    }
-                    // Als er al een level bestaat met dezelfde naam moet deze bijgewerkt worden
-                    else
-                    {
-                        orginele_level.Tekst = level.Tekst;
-                        orginele_level.Moeilijkheidsgraad = level.Moeilijkheidsgraad;
-                        await levelrepository.UpdateAsync(orginele_level);
-                    }
+                    await levelrepository.AddAsync(level);
+                }
+                // Als er al een level bestaat met dezelfde naam moet deze bijgewerkt worden
+                else
+                {
+                    orginele_level.Tekst = level.Tekst;
+                    orginele_level.Moeilijkheidsgraad = level.Moeilijkheidsgraad;
+                    await levelrepository.UpdateAsync(orginele_level);
                 }
             }
+        }
+        private static async Task SeedOefenlevelAsync(IOefenlevelRepository levelrepository, Oefenlevel[] levels)
+        {
+            foreach (Oefenlevel level in levels)
             {
-                var levelrepository = sp.GetRequiredService<IOefenlevelRepository>();
-                Oefenlevel[] levels = [
-                    new Oefenlevel {Id = 0, Naam = "2 vingers", Letteropties = " fj" },
-                    new Oefenlevel {Id = 1, Naam = "4 vingers", Letteropties = " fjdke" },
-                    new Oefenlevel {Id = 2, Naam = "6 vingers", Letteropties = " fjdkewslo" },
-                    new Oefenlevel {Id = 3, Naam = "8 vingers", Letteropties = " ;fjdkewsloa" },
-                    new Oefenlevel {Id = 4, Naam = "letters bovenste rij", Letteropties = " qwertyuiop" },
-                    new Oefenlevel {Id = 5, Naam = "10 vingers", Letteropties = " ;qwertyuiopasdfghjklzxcvbnm" }];
-
-                List<Oefenlevel> levels_in_database = levelrepository.ListAsync().Result;
-                foreach (Oefenlevel level in levels)
+                if (levelrepository.GetByIdAsync(level.Id).Result == null)
                 {
-                    if (levelrepository.GetByIdAsync(level.Id).Result == null)
-                    {
-                        await levelrepository.AddAsync(level);
-                    }
-                    else
-                    {
-                        await levelrepository.UpdateAsync(level);
-                    }
+                    await levelrepository.AddAsync(level);
+                }
+                else
+                {
+                    await levelrepository.UpdateAsync(level);
                 }
             }
         }
