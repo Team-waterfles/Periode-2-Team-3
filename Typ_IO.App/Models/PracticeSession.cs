b686e8510061a8ab76d94f53911201fd32caf076
Rayen87 <119052841+Rayen87@users.noreply.github.com>
2025-12-17 00:12:36 +0100

    Feature/practicemode
    practice mode unlock en locked levels levels toegevoegd

--- Typ_IO.App/Models/PracticeSession.cs
+++ Typ_IO.App/Models/PracticeSession.cs
@@ -1,27 +1,26 @@
 ﻿using System.Collections.Generic;
-using Typ_IO.Core.Models;
 
 namespace BasisJaar2.Models;
 
 public static class PracticeSession
 {
     public static Level? GeselecteerdLevel { get; set; }
-    private static int Voortgang = 0;
+    private static readonly HashSet<int> GehaaldeLevels = new();
 
     public static bool IsLevelUnlocked(int levelNummer)
     {
-        if (levelNummer <= Voortgang + 1) return true;
-        return false;
+        if (levelNummer <= 1) return true;
+        return GehaaldeLevels.Contains(levelNummer - 1);
     }
 
     public static void MarkLevelGehaald(int levelNummer)
     {
-        if (Voortgang < levelNummer)
-        { Voortgang = levelNummer; }
+        if (!GehaaldeLevels.Contains(levelNummer))
+            GehaaldeLevels.Add(levelNummer);
     }
 
     public static bool IsLevelCompleted(int levelNummer)
     {
-        return Voortgang >= levelNummer;
+        return GehaaldeLevels.Contains(levelNummer);
     }
 }
--- Typ_IO.App/Views/Oefening.xaml.cs
+++ Typ_IO.App/Views/Oefening.xaml.cs
@@ -1,26 +1,59 @@
-using BasisJaar2.ViewModels;
 using BasisJaar2.Models;
+using BasisJaar2.ViewModels;
 using Microsoft.Maui.Controls;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Typ_IO.Core.Models;
 
 namespace BasisJaar2.Views
 {
     public partial class Oefening : ContentView
     {
+        private readonly string _levelKey;
         private readonly ContentView _previousPage;
 
-        public Oefening(Level level, ContentView previousPage = null)
+        public static readonly Dictionary<string, string> Voorbeelden = new()
+        {
+            { "Easy", "de snelle bruine vos springt over de luie hond." },
+            { "Medium", "typ deze zin zorgvuldig om je snelheid te testen." },
+            { "Hard", "dit is een moeilijkere oefening voor gevorderde typers." },
+
+            // Levels 1 t/m 10
+            { "1", "fj fjfjf jfff jfjf fjf fjjj ffjf fjfj fjjfff jffj jfj jfjjj fjfffj fffj jfjj" },
+            { "2", "fjdj kfj djek fjek idid kejf fjdkk jfddf kjdfk fjkjd dffjk dkjff jfkdj fdfk dfkjf" },
+            { "3", "wslo slwo fdsj slwo wofs dslj wslo slwo fdsj slwo wofs dslj wofs slwj fdsj wlsj slwo" },
+            { "4", "asdf jkl; asdf jkl; sadf lask fjda ;lkj asdf jkl; sadf lask fjda ;lkj asdf jkla sdfj lkask" },
+            { "5", "qwer tyui opqw type type quit quit write type qwe rtyui opqwe rtyuio pqw ertyuiop qwert yuiopq" },
+            { "6", "asdf jkl; qwer tyui zxcv bnm type snel met alle vingers qaz wsx edc rfvtgb yhnujm kiolp azwsx edcrf" },
+
+            { "7", "kat met pet, vis in glas, jas op kapstok, typ korte woorden snel achter elkaar: kat pet jas vis glas kast tas mat rat lat." },
+            { "8", "vandaag gaan we rustig zinnen typen zonder hoofdletters of leestekens zodat je volledig op het ritme en de aanslagen kunt letten tijdens het oefenen met blind typen" },
+            { "9", "Vandaag typen we zinnen met hoofdletters en komma's. Let goed op spaties, leestekens en ritme, zodat je nauwkeurig én snel leert typen, zonder te veel fouten te maken." },
+            { "10", "Dit is een langere oefening met complete zinnen. Blind typen helpt je om sneller en efficiënter te werken op school, op je werk en thuis. Hoe vaker je oefent, hoe beter je spiergeheugen wordt. Blijf rustig ademen, kijk naar het scherm in plaats van naar het toetsenbord en probeer een gelijkmatig tempo aan te houden." }
+        };
+
+        // Dit is de "hoofd"-constructor die we gebruiken vanuit LevelsPage
+        public Oefening(string key, string source = "play", ContentView previousPage = null)
         {
             InitializeComponent();
 
             _previousPage = previousPage;
+            _levelKey = key ?? string.Empty;
 
-            string voorbeeldTekst = level.Tekst;
+            string voorbeeldTekst = Voorbeelden.ContainsKey(key)
+                ? Voorbeelden[key]
+                : Voorbeelden["1"];
 
-            var vm = new OefeningViewModel(this.Dispatcher, level);
+            // LET OP: alleen 2 parameters, zodat hij matcht met jouw huidige OefeningViewModel
+            var levelObj = new Level
+            {
+                Nummer = _levelKey,
+                Id = int.TryParse(_levelKey, out var id) ? id : 0,
+                Naam = PracticeSession.GeselecteerdLevel?.Naam ?? $"Level {_levelKey}",
+                Tekst = voorbeeldTekst
+            };
+
+            var vm = new OefeningViewModel(this.Dispatcher, levelObj);
 
             if (_previousPage is LevelsPage)
                 vm.PracticeModeHints = true;
@@ -32,6 +65,12 @@ namespace BasisJaar2.Views
                 await Task.Delay(50);
                 HiddenEditor?.Focus();
             });
+        }
+
+        // Extra constructor zodat oude code new Oefening(level, currentPage) blijft werken
+        public Oefening(Level level, ContentView previousPage = null)
+            : this(level?.Nummer ?? "1", "play", previousPage)
+        {
         }
 
         private OefeningViewModel VM => BindingContext as OefeningViewModel;
@@ -59,10 +98,19 @@ namespace BasisJaar2.Views
 
         private void Terug_Clicked(object sender, EventArgs e)
         {
+            // ? Refresh levels zodat icons/knoppen updaten
+            if (_previousPage is LevelsPage levelsPage)
+            {
+                if (levelsPage.BindingContext is LevelsViewModel vm)
+                    vm.RefreshLevelStates();
+            }
+
+            // ? Gewoon terug navigeren
             if (_previousPage != null && MainPageViewModel.Current != null)
                 MainPageViewModel.Current.SubpageContent = _previousPage;
             else if (MainPageViewModel.Current != null)
                 MainPageViewModel.Current.SubpageContent = new MoeilijkheidsgraadPage();
         }
+
     }
 }
